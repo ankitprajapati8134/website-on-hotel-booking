b6aa8cf5b697aaec5425ded46b55eac5046a8eef
Ankit Prajapati <ankitprajapati8134@gmail.com>
2017-11-22 15:19:24 +0530

    Introduction.php

---
+++ Introduction.php
@@ -0,0 +1,31 @@
+<?php include 'header.php';?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Introduction</title>
+	
+</head>
+<body>
+<div class="container">
+
+       <h1 class="title">Introduction</h1>
+       <div class="row">
+              <div class="col-sm-4"><p>The elegant Conrad Residences are luxurious serviced apartments located within the Conrad Bangkok hotel. These sophisticated apartments lie within the prestigious All Seasons Place near the city's premier shopping and commercial centers. Experience the freedom and privacy of beautifully furnished apartments combined with the luxurious amenities of the hotel.</p></div>
+              <div class="col-sm-4"><p>Indulge in rejuvenating facials, body treatments and relaxing massages in one of Seasons Spa's 11 tranquil treatment rooms. Stay active in the outdoor pool, two floodlit tennis courts, fully equipped 24-hour Bodyworx gym and rooftop running track - all located on the seventh floor.</p></div>
+              <div class="col-sm-4"><p>Dry stout aau pilsner. bock cold filter lauter tun specific gravity degrees plato ibu cold filter. brew kettle crystal malt, hard cider ipa bright beer caramel malt. pitching aerobic keg finishing hops pub units of bitterness.</p></div>              
+       </div>
+
+
+       <div class="spacer">
+       <div class="embed-responsive embed-responsive-16by9">
+       	<img src="images/11.jpg" alt="images/1.jpg">
+       </div>
+       </div>
+
+
+
+
+</div>
+</body>
+</html>
+<?php include 'footer.php';?>
